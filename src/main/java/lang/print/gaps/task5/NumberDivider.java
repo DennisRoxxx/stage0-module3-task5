d01edf8dbed59de24a84f3d9b9d7389cfc2191f4
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2022-12-07 11:17:18 +0300

    Update NumberDivider.java

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        toBeDivided = 19;
+        float div = toBeDivided/5f;
+        System.out.println(div);
+
     }
 }
